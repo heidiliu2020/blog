bbb5f5f186c53171a2ad2387d43e027487bcb6ce
heidi.liu <heidi.liu@ubitus.net>
2024-04-09 17:03:02 +0800

    update: remote fetch

---
+++ my-blog/src/pages/home-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <h1>Home page</h1>
+      <p>This is a React page</p>
+    </Layout>
+  );
+}
